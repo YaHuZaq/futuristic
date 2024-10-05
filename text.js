1480db4c8f3ca4fab9e7531cc3d98522857727a9
YaHuZaq <150091897+YaHuZaq@users.noreply.github.com>
2024-10-04 17:11:10 -0700

    Add files via upload

---
+++ text.js
@@ -0,0 +1,11 @@
+function showAll(){
+    document.getElementById("cancel").style.display= 'block';
+    document.getElementById("navs").style.display = 'flex';
+    document.getElementById("side").style.display = 'none';
+}
+
+function hideAll(){
+    document.getElementById("cancel").style.display= 'none';
+    document.getElementById("navs").style.display = 'none';
+    document.getElementById("side").style.display = 'block';
+}
